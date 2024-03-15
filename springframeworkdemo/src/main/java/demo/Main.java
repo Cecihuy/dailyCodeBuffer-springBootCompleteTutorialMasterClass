21ae0581c25f24e2f1eb7ed67175ae212937b654
gitKraken <gitkraken@mail.id>
2024-03-15 01:37:25 -0400

    create Nurse.class and call it by id (26:54-28:23)

--- springframeworkdemo/src/main/java/demo/Main.java
+++ springframeworkdemo/src/main/java/demo/Main.java
@@ -1,12 +1,11 @@
 package demo;
-
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring.xml");
-        Doctor doctor = applicationContext.getBean(Doctor.class);
+        Nurse doctor = (Nurse) applicationContext.getBean("nurse");
         doctor.assist();
     }
 }
