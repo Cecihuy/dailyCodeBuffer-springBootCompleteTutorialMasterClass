6e6799a7829ac06edc4c71d97cd7307d7de0dc9a
gitKraken <gitkraken@mail.id>
2024-03-15 01:28:18 -0400

    create spring.xml file and run first time (22:13-26:54)

--- springframeworkdemo/src/main/java/demo/Main.java
+++ springframeworkdemo/src/main/java/demo/Main.java
@@ -1,8 +1,12 @@
 package demo;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
-        Doctor doctor = new Doctor();
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring.xml");
+        Doctor doctor = applicationContext.getBean(Doctor.class);
         doctor.assist();
     }
 }
