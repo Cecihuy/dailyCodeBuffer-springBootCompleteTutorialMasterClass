c5f0eb89931a76a0de3eaa1c5682befb1a490210
gitKraken <gitkraken@mail.id>
2024-03-18 05:40:12 -0400

    some JoinPoint operation (1:15:42-1:18:40)

--- springaopdemo/src/main/java/demo/LoggingAspect.java
+++ springaopdemo/src/main/java/demo/LoggingAspect.java
@@ -1,4 +1,5 @@
 package demo;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
@@ -8,8 +9,10 @@ import org.springframework.stereotype.Component;
 @Aspect
 public class LoggingAspect {
     @Before("execution(* demo.ShoppingCart.checkout(..))")
-    public void beforeLogger(){
-        System.out.println("before logger method called");
+    public void beforeLogger(JoinPoint joinPoint){
+        System.out.println(joinPoint.getSignature());
+        String arg = joinPoint.getArgs()[0].toString();
+        System.out.println("before logger method called with Argument : " + arg);
     }
     @After("execution(* *.checkout(..))")
     public void afterLogger(){
