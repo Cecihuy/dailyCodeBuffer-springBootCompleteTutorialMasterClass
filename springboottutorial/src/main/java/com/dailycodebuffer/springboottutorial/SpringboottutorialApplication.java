bba2858f1fe792a0198808c175ff2d2446602459
gitKraken <gitkraken@mail.id>
2024-03-19 00:39:46 -0400

    add dependency (springboot session start here) (1:22:38-1:47:43)

---
+++ springboottutorial/src/main/java/com/dailycodebuffer/springboottutorial/SpringboottutorialApplication.java
@@ -0,0 +1,10 @@
+package com.dailycodebuffer.springboottutorial;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringboottutorialApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringboottutorialApplication.class, args);
+	}
+}
