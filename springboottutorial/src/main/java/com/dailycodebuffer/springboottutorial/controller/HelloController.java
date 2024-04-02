4cebd0b3cd8ff1f324a0accec3308d5d3228ae7a
gitKraken <gitkraken@mail.id>
2024-04-02 02:11:55 -0400

    using custom key value pair from application.properties (4:00:03-4:03:38)

--- springboottutorial/src/main/java/com/dailycodebuffer/springboottutorial/controller/HelloController.java
+++ springboottutorial/src/main/java/com/dailycodebuffer/springboottutorial/controller/HelloController.java
@@ -1,12 +1,16 @@
 package com.dailycodebuffer.springboottutorial.controller;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    @Value(value = "${welcome.message}")
+    private String welcomeMessage;
+
     @RequestMapping(path = "/", method = RequestMethod.GET)
     public String helloWorld(){
-        return "Welcome to Daily Code Buffer!!!";
+        return welcomeMessage;
     }
 }
