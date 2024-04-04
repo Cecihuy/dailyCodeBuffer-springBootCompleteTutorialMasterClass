58c6fcf650bb3888735d65ca137fbb04f5fe00e3
gitKraken <gitkraken@mail.id>
2024-04-04 00:23:43 -0400

    naming table and column, add uniqueConstraint, set studentId to be auto generated, make sure emailId is not null (4:52:00-5:00:46)

--- springdatajpatutorial/src/main/java/com/dailycodebuffer/springdatajpatutorial/entity/Student.java
+++ springdatajpatutorial/src/main/java/com/dailycodebuffer/springdatajpatutorial/entity/Student.java
@@ -1,13 +1,37 @@
 package com.dailycodebuffer.springdatajpatutorial.entity;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 
 @Entity
+@Table(
+    name = "tbl_student",
+    uniqueConstraints = 
+        @UniqueConstraint(
+            name = "emailid_unique",
+            columnNames = "email_address"
+        )
+)
 public class Student {
     @Id
+    @SequenceGenerator(
+        name = "student_sequence",
+        sequenceName = "student_sequence",
+        allocationSize = 1
+    )
+    @GeneratedValue(
+        strategy = GenerationType.SEQUENCE,
+        generator = "student_sequence"
+    )
     private Integer studentId;
     private String firstName;
     private String lastName;
+    @Column(name = "email_address", nullable = false)
     private String emailId;
     private String guardianName;
     private String guardianEmail;
