f941a2871263cc8f88fe32f994f37d0afe07c172
gitKraken <gitkraken@mail.id>
2024-04-03 23:50:15 -0400

    add dependency (spring data jpa session start here) (4:33:17-4:46:17)

---
+++ springdatajpatutorial/src/main/java/com/dailycodebuffer/springdatajpatutorial/SpringdatajpatutorialApplication.java
@@ -0,0 +1,10 @@
+package com.dailycodebuffer.springdatajpatutorial;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdatajpatutorialApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdatajpatutorialApplication.class, args);
+	}
+}
